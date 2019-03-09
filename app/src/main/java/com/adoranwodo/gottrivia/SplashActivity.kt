16624dec2c0e632af61243fea43000b1f0a67d81
nennenwodo <nennenwodo@gmail.com>
2019-03-09 08:53:23 +0100

    Added splash screen

--- app/src/main/java/com/adoranwodo/gottrivia/SplashActivity.kt
+++ app/src/main/java/com/adoranwodo/gottrivia/SplashActivity.kt
@@ -1,12 +1,47 @@
 package com.adoranwodo.gottrivia
 
+import kotlinx.android.synthetic.main.activity_splash.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.content.Intent
 
 class SplashActivity : AppCompatActivity() {
+    private var interval: Long = 100
+    private var count: Int = 5
+    private var mProgress: ProgressBar ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        mProgress = splash_screen_progress_bar
+
+        // Start lengthy operation in a background thread
+        Thread(Runnable {
+            showProgressBar()
+            goToHome()
+            finish()
+        }).start()
+    }
+
+    private fun showProgressBar() {
+        var progress = 0
+        while (progress < 105) {
+            try {
+                //increment progress bar every 100ms
+                Thread.sleep(interval)
+                mProgress?.progress = progress
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            progress += count
+        }
+    }
+
+    private fun goToHome() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
     }
 }
